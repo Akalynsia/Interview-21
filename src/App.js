52ff850188ad407e5318e60154b37cbda28b6074
Akalynsia <arminasmirage@gmail.com>
2024-04-20 21:31:36 +0300

    interview

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Link, Routes, Route, Navigate } from "react-router-dom";
 import classNames from "classnames";
